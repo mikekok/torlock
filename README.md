84dc55b9a5ca2f9395d0b4753c937417e0b6ba96
Mike Kokkolios <mikekokkolios@gmail.com>
2017-06-20 13:39:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# torlock
+A Node.js module to search for torrents at Torlock tracker.
+
+## Installation
+* `npm install torlock`
+
+## Usage
+```javascript
+const torlock = require('torlock')
+
+torlock.search(keyword, function(err, results) {
+  console.log(results) // returns name, seeders, leechers, url
+})
+```
+
+## Packages
+* [cloudscraper](https://github.com/codemanki/cloudscraper)
+* [cheerio](https://github.com/cheeriojs/cheerio)
+
+## License
+This project is licensed under The MIT License (MIT). Which means that you can use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software. But you always need to state that xWeb is the original author of this template.
+
+Project is developed and maintained by [xWeb](https://xweb.gr/).
---
+++ index.js
@@ -0,0 +1,28 @@
+// Modules
+const cloudscraper = require('cloudscraper')
+const cheerio = require('cheerio')
+
+// Torlock.com base URL
+let torlockURL = 'https://www.torlock.com'
+
+module.exports = {
+  search: function(keyword, cb) {
+    let torrents = []
+    var reqURL = torlockURL + '/all/torrents/' + keyword + '.html'
+    cloudscraper.get(torlockURL, function(error, response, body) {
+      var $ = cheerio.load(body)
+      $('table:nth-child(2) tr').each(function(index, el) {
+        var torrent = {}
+        torrent.name = $(this).find('td:nth-child(1) b').text()
+        console.log(torrent.name)
+        torrent.seeders = $(this).find('td:nth-child(4)').text()
+        torrent.leechers = $(this).find('td:nth-child(5)').text()
+        torrent.url = torlockURL + $(this).find('td:nth-child(1) a').attr('href')
+        if (torrent.name !== '') {
+          torrents.push(torrent)
+        }
+      })
+      return cb(null, torrents)
+    })
+  }
+}
